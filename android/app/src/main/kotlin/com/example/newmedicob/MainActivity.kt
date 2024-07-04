ccb99b02623861d610b0f244b93c2736f8c2b41c
Ampaw24 <ampawsafo22@gmail.com>
2024-07-04 14:30:49 +0000

    initial commit

---
+++ android/app/src/main/kotlin/com/example/newmedicob/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.newmedicob
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'core/theme/theme_helper.dart';
+import 'presentation/unboarding/splashscreen.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'MedicobApp',
+      theme: theme,
+      home:const SplashScreenPage(),
+    );
+  }
+}
+
